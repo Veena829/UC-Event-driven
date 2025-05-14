8f15d845c97cb2f4077fd7956468902f90f41923
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-14 15:59:17 +0530

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -49,8 +49,6 @@ resource "aws_api_gateway_deployment" "this" {
 ]
 }
 
-
 output "api_gateway_url" {
-  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/${var.api_gateway_stage_name}/resource"
+  value = aws_api_gateway_deployment.this.invoke_url
 }
-
