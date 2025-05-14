6007a6614b5229dcd22e7174a6e03e024a34058c
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-14 17:19:08 +0530

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,5 +1,6 @@
 resource "aws_api_gateway_rest_api" "api" {
-  name = "myApi_SQS"
+  name        = "myApi_SQS"
+  description = "My API that integrates with SQS"
 }
 
 resource "aws_api_gateway_resource" "root" {
